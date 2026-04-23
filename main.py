1f967e2deaa1a743be35c2dcb64d4f2cbc70d41a
Ali Mohammadi <alimohammadi02321@gmail.com>
2026-04-23 16:24:00 +0330

    Show tokens and prompt only on verbose mode

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
-
-
-
+from google.genai import types
 
 def main():
-
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
@@ -14,9 +12,22 @@ def main():
     # for m in client.models.list():
     #     print(m.name)
 
+    if (len(sys.argv) < 2):
+        print("I need prompt")
+        sys.exit(2)
+
+    verbose_flag = False
+    if (len(sys.argv) == 3 and sys.argv[2] == "--verbose"):
+        verbose_flag = True
+
+    
+    prompt = sys.argv[1]
+    
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+
     response = client.models.generate_content(
         model="gemini-2.5-flash-lite",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=messages
     )
 
     print(response.text)
@@ -24,7 +35,9 @@ def main():
     if response is None or response.usage_metadata is None:
         print("Response is malformed")
         return
-    else:
+    
+    if verbose_flag:
+        print(f"User prompt: {prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
