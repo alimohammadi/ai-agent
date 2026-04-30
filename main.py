cf5f9a6e416a8027664612fe0720d05c24c42a75
Ali Mohammadi <alimohammadi02321@gmail.com>
2026-05-01 00:15:54 +0330

    Add ability to llm to get files info with function

--- main.py
+++ main.py
@@ -3,11 +3,24 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from functions.get_files_info import get_files_info
+from functions.get_files_info import schema_get_files_info
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
+
+    system_prompt = """
+        You are a helpful AI coding agent.
+
+        When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+        - List files and directories
+        - Read file contents
+        - Execute Python files with optional arguments
+        - Write or overwrite files
+
+        All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
 
     client = genai.Client(api_key=api_key)
     # for m in client.models.list():
@@ -26,12 +39,19 @@ def main():
     
     messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash-lite",
-        contents=messages
+    available_functions = types.Tool(
+        function_declarations=[schema_get_files_info],
     )
 
-    print(response.text)
+    config = types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+    )
+    
+    response = client.models.generate_content(
+        model="gemini-2.5-flash-lite",
+        contents=messages,
+        config=config
+    )
 
     if response is None or response.usage_metadata is None:
         print("Response is malformed")
@@ -42,8 +62,13 @@ def main():
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:     
+        print(response.text)
 
-# main()
+main()
 
 
-print(get_files_info("calculator", "pkg"))
+# print(get_files_info("calculator", "pkg"))
